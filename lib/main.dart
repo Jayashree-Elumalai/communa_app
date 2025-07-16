5dbe19055c8d80b5d71fed6a5cbb710c9a9e9ba5
WongYeeRou <133182224+WongYeeRou@users.noreply.github.com>
2025-07-16 13:28:45 +0800

    Update main.dart
    1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 import 'screens/login_pg.dart';
+import 'screens/register_pg.dart';
 import 'screens/home_pg.dart';
+import 'screens/booking_pg.dart';
+import 'screens/feedback_pg.dart';
+import 'screens/admin_pg.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,10 +24,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Communa App',
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: const Color(0xFFF9FAFB),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/admin': (context) => const AdminPanelScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/booking': (context) => const BookingPage(),
+        '/feedback': (context) => const FeedbackPage(),
+      },
     );
   }
 }
-
